e380944f914e886f66d426160b3133d5f4785ad0
caiqidong <863828278@qq.com>
2025-08-19 10:28:56 +0800

    打包配置

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from './components/button';
+// ...其他组件
+// export * from './utils/format';
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,85 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
-import path from 'node:path';
+import path, { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({
+    tsconfigPath: 'tsconfig.build.json',
+    outDir: './dist/es',
+    entryRoot: './src',
+    exclude: ['node_modules', 'tests', 'vite.config.ts', 'main.tsx', 'App.tsx'],
+  }),
+  dts({
+    tsconfigPath: 'tsconfig.build.json',
+    outDir: './dist/lib',
+    entryRoot: './src',
+    exclude: ['node_modules', 'tests', 'vite.config.ts', 'main.tsx', 'App.tsx'],
+  })],
+  build: {
+    minify: false,
+    emptyOutDir: true,
+    cssCodeSplit: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'car-mobile-react',
+      fileName: format => `index.${format}.js`,
+      // formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime',
+        'react/jsx-dev-runtime'],
+      output: [
+        {
+          // es 产物配置
+          format: 'es',
+          entryFileNames: '[name].js',
+          exports: 'named',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          dir: './dist/es',
+        },
+        {
+          // cjs 产物配置
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          exports: 'named',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          dir: './dist/lib',
+        },
+        {
+          // umd 产物配置
+          format: 'umd',
+          entryFileNames: 'index.js',
+          exports: 'named',
+          name: 'car-mobile-react',
+          dir: './dist/umd',
+          globals: {
+            'react': 'React',
+            'react-dom': 'ReactDOM',
+            'react/jsx-runtime': 'jsxRuntime'
+          }
+        },
+      ],
+    },
+  },
   test: {
     projects: [{
       extends: true,
       plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
+        // The plugin will run tests for the stories defined in your Storybook config
+        // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+        storybookTest({
+          configDir: path.join(dirname, '.storybook')
+        })],
       test: {
         name: 'storybook',
         browser: {
